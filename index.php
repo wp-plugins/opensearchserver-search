c06674534d9ed4374ea223649394ea072125977d
ekeller <ekeller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-11 20:01:24 +0000

    Fix the version number in index.php
    git-svn-id: https://plugins.svn.wordpress.org/opensearchserver-search/trunk@611091 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  Author: Emmanuel Keller - Naveen.A.N
  Author URI: http://open-search-server.com
  Tested up to: 3.4.2
- Version:1.1.2
+ Version:1.2.0
  */
 require_once 'lib/oss_api.class.php';
 require_once 'lib/oss_misc.lib.php';
